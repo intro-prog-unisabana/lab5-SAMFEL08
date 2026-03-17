2c0d3372fc48f9d96cc1d4003a7e190cc99d01dc
Samuel G <gonzalezsam451@gmail.com>
2026-03-16 21:36:50 -0500

    prueba 7

--- caller.py
+++ caller.py
@@ -1,4 +1,10 @@
+from mystery_module import transform_data
 
+x = int(input("Enter x: "))
+y = float(input("Enter y: "))
+result = transform_data(x, y, "quiz_test")
+
+print(result)
 
 # FREEZE CODE BEGIN
 x = int(input())
---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,2 @@
+def celsius_to_fahrenheit(temp: float) -> float:
+    return (temp * 9/5) + 32
